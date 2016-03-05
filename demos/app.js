ec3f413e429aec7b12c18098c7828c2deb83d72a
sunhao07 <sunhao07@meituan.com>
2016-03-05 14:53:49 +0800

    add entry for test

--- demos/app.js
+++ demos/app.js
@@ -4,7 +4,12 @@ import send from 'koa-send';
 let app = koa();
 
 app.use(function *() {
-    yield send(this, this.path);
+    let path = this.path;
+    if (path === '/') {
+        this.response.redirect('/demos/index.html');
+    } else {
+        yield send(this, path);
+    }
 });
 
 app.listen(9527);
